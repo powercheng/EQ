50d036d2e00670e9bffb7c54ef8f0f9db06e7696
peng cheng <pengchengusc@gmail.com>
2016-10-07 01:01:46 -0400

    1123444

--- app/src/main/java/com/example/peng/eq/MainActivity.java
+++ app/src/main/java/com/example/peng/eq/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.peng.eq;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.parse.Parse;
+import com.parse.ParseObject;
+import com.parse.ParseUser;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("Your Back4app application ID")
+                .clientKey("You can found it on b4a core setting")
+                .server("https://parseapi.back4app.com/").build()
+        );
         ParseObject testObject = new ParseObject("TestObject");
         testObject.put("foo", "bar");
         testObject.saveInBackground();
