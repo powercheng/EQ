55caac01511dfd4dfb74c600075a28a9a835a329
peng cheng <pengchengusc@gmail.com>
2016-10-06 23:41:03 -0400

    1123444

--- app/src/main/java/com/example/peng/eq/MainActivity.java
+++ app/src/main/java/com/example/peng/eq/MainActivity.java
@@ -5,10 +5,13 @@ import android.os.Bundle;
 
 
 public class MainActivity extends AppCompatActivity {
-    //122334
+    //1223345678
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        ParseObject testObject = new ParseObject("TestObject");
+        testObject.put("foo", "bar");
+        testObject.saveInBackground();
         setContentView(R.layout.activity_main);
     }
 
