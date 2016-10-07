c64bd5ba9d48f95cde2201720cfc5c94bac262dc
peng cheng <pengchengusc@gmail.com>
2016-10-06 23:21:57 -0400

    1123

--- app/src/main/java/com/example/peng/eq/MainActivity.java
+++ app/src/main/java/com/example/peng/eq/MainActivity.java
@@ -3,8 +3,9 @@ package com.example.peng.eq;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
 
+public class MainActivity extends AppCompatActivity {
+    //122334
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
