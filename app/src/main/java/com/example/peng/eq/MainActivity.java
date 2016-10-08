b64d6ad4fbe987b58b117b6af98e54fe3ecddae5
SusanWoo <cizhen@email.sc.edu>
2016-10-08 11:51:45 -0400

    Signin, login view

--- app/src/main/java/com/example/peng/eq/MainActivity.java
+++ app/src/main/java/com/example/peng/eq/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
 
 import com.parse.Parse;
 import com.parse.ParseObject;
@@ -14,18 +15,24 @@ import com.parse.ParseUser;
 
 
 public class MainActivity extends AppCompatActivity {
+    private static boolean isParseInitialized = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        Parse.initialize(new Parse.Configuration.Builder(this)
-                .applicationId("5mUU6pV0Ljoc3Er2ZrJoUUBC93kBeEfuvu2vkIUw")
-                .clientKey("0vj8zObedfUjbtTc92cX8TIR2ccEEhlIdgfvf5BG")
-                .server("https://parseapi.back4app.com/").build()
-        );
+        if(!isParseInitialized){
+            Parse.initialize(new Parse.Configuration.Builder(this)
+                    .applicationId("5mUU6pV0Ljoc3Er2ZrJoUUBC93kBeEfuvu2vkIUw")
+                    .clientKey("0vj8zObedfUjbtTc92cX8TIR2ccEEhlIdgfvf5BG")
+                    .server("https://parseapi.back4app.com/").build()
+            );
+            isParseInitialized = true;
+        }
+
 //        ParseObject testObject = new ParseObject("Event");
 //        testObject.put("title", "bar");
 //        testObject.saveInBackground();
         super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_main);
     }
 
